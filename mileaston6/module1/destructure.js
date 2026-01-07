d7e30def24fc1f2f92d1e9993c1a1e2db60a7227
mdfoyezkhan <mdfoyezkn@gmail.com>
2026-01-07 22:45:01 +0600

    destructure.js file added in this commit

---
+++ mileaston6/module1/destructure.js
@@ -0,0 +1,10 @@
+const friends ={
+    name:'salman kham',
+    age: 23,
+    propetion:'suppert staf',
+}
+console.log(friends.name)
+const age = friends.age;
+console.log(age)
+const {name, ages, propetion}= friends;
+console.log(propetion)
