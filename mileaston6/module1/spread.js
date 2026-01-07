519dfdd49429b453c75311049c8da408789e0b50
mdfoyezkhan <mdfoyezkn@gmail.com>
2026-01-07 07:36:48 +0600

    new practice spread oparetore

--- mileaston6/module1/spread.js
+++ mileaston6/module1/spread.js
@@ -4,3 +4,8 @@
 
 const numbers = [1, 2, 3, 4 ,5 ,6];
 console.log(Math.max(...numbers))
+
+const friends = ['foyej khan', 'salman khan', 'emon khan'];
+console.log(...friends)
+const newArray = ['najmul khan', ...friends]
+console.log(...newArray)
