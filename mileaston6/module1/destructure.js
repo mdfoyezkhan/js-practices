5bb3bd3d1e0bb5335136b0399679720a853d1464
mdfoyezkhan <mdfoyezkn@gmail.com>
2026-01-07 22:49:55 +0600

    new commit

--- mileaston6/module1/destructure.js
+++ mileaston6/module1/destructure.js
@@ -8,3 +8,8 @@ const age = friends.age;
 console.log(age)
 const {name, ages, propetion}= friends;
 console.log(propetion)
+
+
+const person = ['salman', 'foyez', 'emon'];
+const [first, second, therd] = person;
+console.log(first , second, therd)
