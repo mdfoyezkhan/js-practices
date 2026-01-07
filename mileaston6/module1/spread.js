297744b9a4688a23ba0079b3b717c2387b2f1661
mdfoyezkhan <mdfoyezkn@gmail.com>
2026-01-07 07:30:41 +0600

    spread.js file added in this commit

---
+++ mileaston6/module1/spread.js
@@ -0,0 +1,6 @@
+// const friends = ['foyej khan', 'salman khan', 'emom khan'];
+// const newArray = ['najmul', ...friends]
+// console.log(newArray)
+
+const numbers = [1, 2, 3, 4 ,5 ,6];
+console.log(Math.max(...numbers))
