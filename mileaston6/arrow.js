47cd7a1879361124e48d60eb13ec3f18dd2aec99
mdfoyezkhan <mdfoyezkn@gmail.com>
2026-01-07 06:44:51 +0600

    this commit for function

---
+++ mileaston6/arrow.js
@@ -0,0 +1,7 @@
+const name = () => console.log('foyej khan');
+console.log(name())
+const square = x => x * x;
+console.log(square(10))
+document.getElementById('hello').addEventListener('click', function(){
+    console.log('hello')
+})
